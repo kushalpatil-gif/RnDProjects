eac3abdb831dcf581638ddda40479edace37e5f2
kushalpatil-gif <kushal.patiloth@gmail.com>
2026-02-05 04:39:04 +0000

    Add fyers_config.example.py

---
+++ fyers_bot/fyers_config.example.py
@@ -0,0 +1,12 @@
+config = {
+    'API_client_id': 'HJKLLJPLIV-100',
+    'API_secret_key': 'LG1I44L1UO',
+    'redirect_uri': 'http://localhost:8080/oauth2callback',
+    'response_type': 'code',
+    'grant_type': 'authorization_code',
+    'fy_client_id': 'XK16109',
+    'fy_client_pwd': '',
+    'pin': '0000',
+    'access_token_path': '/home/ubuntu/.openclaw/workspace/fyers_bot/access_token.json',
+    'log_path': '/home/ubuntu/.openclaw/workspace/fyers_bot/fyers.log'
+}
